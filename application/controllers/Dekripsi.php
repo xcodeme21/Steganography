7290d4d8c6e834cda6f7f5499cb0323ce784e195
Agus Siswanto <agus.siswanto@erajaya.com>
2023-06-27 09:06:33 +0700

    dekripsi update

--- application/controllers/Dekripsi.php
+++ application/controllers/Dekripsi.php
@@ -31,41 +31,84 @@ class Dekripsi extends CI_Controller {
 
 	public function process() {
 		$this->load->library('form_validation');
-
 		$this->form_validation->set_rules('private_key', 'Private Key', 'required|callback_validate_rsa_private_key');
 
 		if ($this->form_validation->run() == FALSE) {
-			// Validasi gagal, tampilkan pesan error
+			// Validation failed, display error message
 			$this->session->set_flashdata('error', validation_errors());
 			redirect('dekripsi');
 		} else {
-			// Validasi berhasil, lanjutkan dengan proses upload gambar
+			// Validation succeeded, continue with image upload and decryption
 
-			// Konfigurasi upload
-			$config['upload_path'] = APPPATH . 'uploads/'; // Tentukan folder penyimpanan image
-			$config['allowed_types'] = 'gif|jpg|jpeg|png'; // Tentukan jenis file yang diizinkan
-			$config['max_size'] = 2048; // Tentukan ukuran maksimum file (dalam kilobita)
+			// Configuration for file upload
+			$config['upload_path'] = APPPATH . 'uploads/'; // Specify the folder for storing the image
+			$config['allowed_types'] = 'gif|jpg|jpeg|png'; // Specify the allowed file types
+			$config['max_size'] = 2048; // Specify the maximum file size (in kilobytes)
 
-			// Load library Upload dan inisialisasi konfigurasi
+			// Load Upload library and initialize the configuration
 			$this->load->library('upload', $config);
 
 			if (!$this->upload->do_upload('image')) {
-				// Jika proses upload gagal, tampilkan pesan error
+				// If the upload process fails, display error message
 				$error = $this->upload->display_errors();
 				$this->session->set_flashdata('error', $error);
 				redirect('dekripsi');
 			} else {
-				// Jika proses upload berhasil, ambil informasi file yang diupload
+				// If the upload process succeeds, get the uploaded file information
 				$uploadData = $this->upload->data();
 
-				// Dapatkan path file yang diupload
-				$filePath = 'uploads/' . $uploadData['file_name'];
+				// Get the file path of the uploaded image
+				$filePath = $uploadData['full_path'];
 
-				$this->session->set_flashdata('success', "Image berhasil diupload. Path file: " . $filePath);
+				// Perform decryption using the private key
+				$privateKey = $this->input->post('private_key');
+				$decryptedFilePath = $this->decryptImage($filePath, $privateKey);
+
+				if ($decryptedFilePath) {
+					$this->session->set_flashdata('success', "Image successfully decrypted. Decrypted file path: " . $decryptedFilePath);
+				} else {
+					$this->session->set_flashdata('error', "Failed to decrypt the image.");
+				}
+
+				// Redirect back to the decryption form
 				redirect('dekripsi');
 			}
 		}
 	}
+
+	private function decryptImage($filePath, $privateKey) {
+		// Parse the private key values
+		$privateKey = trim($privateKey, '()');
+		list($d, $n) = explode(',', $privateKey);
+
+		// Read the stego image data
+		$imageData = file_get_contents($filePath);
+
+		// Decrypt the stego image using RSA decryption algorithm
+		$decryptedData = ''; // Initialize the variable to store the decrypted image data
+
+		// Perform RSA decryption on each pixel/byte of the stego image
+		for ($i = 0; $i < strlen($imageData); $i++) {
+			$byte = ord($imageData[$i]); // Get the ASCII value of the byte
+
+			// Perform RSA decryption on the byte using the private key
+			$decryptedByte = bcpowmod($byte, $d, $n); // Use appropriate RSA decryption function
+
+			$decryptedData .= chr($decryptedByte); // Append the decrypted byte to the decrypted data
+		}
+
+		// Save the decrypted image data to a new file
+		$decryptedFilePath = APPPATH . 'uploads/decrypted_image.jpg'; // Replace with the actual path and filename of the decrypted image
+		file_put_contents($decryptedFilePath, $decryptedData);
+
+		if (file_exists($decryptedFilePath)) {
+			return $decryptedFilePath;
+		} else {
+			return false;
+		}
+	}
+
+
 
 	public function validate_rsa_private_key($str) {
 		if (preg_match('/^\(\d+,\d+\)$/', $str) !== 1) {
