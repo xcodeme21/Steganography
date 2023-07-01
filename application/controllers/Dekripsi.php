d1103509ac3948751f7e68a36932ec408ec857fa
Agus Siswanto <agus.siswanto@erajaya.com>
2023-07-01 12:50:59 +0700

    update dekrips

--- application/controllers/Dekripsi.php
+++ application/controllers/Dekripsi.php
@@ -1,10 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
-
 class Dekripsi extends CI_Controller {
     public function __construct() {
         parent::__construct();
@@ -26,7 +22,7 @@ class Dekripsi extends CI_Controller {
 
     public function sessionValidate() {
         if (!$this->session->userdata('nama')) {
-            $this->session->set_flashdata('error', "Session habis");
+            $this->session->set_flashdata('error', 'Session habis');
             redirect('/login'); // Ganti '/login' dengan URL sebenarnya tempat Anda ingin mengarahkan pengguna
         }
     }
@@ -34,51 +30,68 @@ class Dekripsi extends CI_Controller {
     public function process() {
         // Mendapatkan data dari formulir
         $privateKey = $this->input->post('private_key');
-        
+
         // Validasi format private_key (misalnya: (1234,1378))
         if (!preg_match('/^\(\d+,\d+\)$/', $privateKey)) {
-            echo "Format private_key tidak valid. Harap masukkan format yang benar, misalnya: (1234,1378)";
+            $this->session->set_flashdata('error', 'Format private_key tidak valid. Harap masukkan format yang benar, misalnya: (1234,1378)');
+            redirect('dekripsi');
             return;
         }
-        
+
         // Memuat file gambar
         $imageFile = $_FILES['image']['tmp_name'];
-        
-        // Membaca file gambar menggunakan PhpSpreadsheet
-        $spreadsheet = IOFactory::load($imageFile);
-        
+
         // Mendapatkan pesan yang disisipkan dari gambar
-        $hiddenMessage = $this->getHiddenMessage($spreadsheet);
-        
-        // Mendekripsi pesan menggunakan private_key
-        $decryptedMessage = $this->decryptWithRSA($hiddenMessage, $privateKey);
-        
-        // Menyimpan pesan ke dalam file teks
-        $outputFile = APPPATH . 'uploads/decrypt/output.txt';
-        write_file($outputFile, $decryptedMessage); // Menggunakan File Helper untuk menulis file
-        
+        $hiddenMessage = $this->getHiddenMessage($imageFile);
+
         // Menampilkan pesan sukses atau error
-        echo "Dekripsi dan ekstraksi berhasil! Pesan telah disimpan dalam file output.txt";
+        if ($hiddenMessage !== false) {
+            $this->session->set_flashdata('hidden_message', $hiddenMessage);
+            $this->session->set_flashdata('success', 'Berhasil mengekstrak pesan tersembunyi dari gambar.');
+            redirect('dekripsi');
+        } else {
+            $this->session->set_flashdata('error', 'Gagal mengekstrak pesan tersembunyi dari gambar.');
+            redirect('dekripsi');
+        }
     }
-    
-    private function getHiddenMessage($spreadsheet) {
-        $worksheet = $spreadsheet->getActiveSheet();
-        $drawing = $worksheet->getDrawingCollection()[0];
-        
-        // Mendapatkan deskripsi/pesan yang disisipkan dalam gambar
-        $hiddenMessage = $drawing->getDescription();
-        
-        return $hiddenMessage;
-    }
-    
-    private function decryptWithRSA($encryptedMessage, $privateKey) {
-        // Implementasikan logika Anda di sini untuk mendekripsi pesan menggunakan private_key RSA
-        // ...
-        // Misalnya, Anda dapat menggunakan library RSA yang tersedia atau mengimplementasikan algoritma RSA sendiri
-        
-        // Contoh sederhana untuk mendekripsi pesan dengan private_key RSA
-        $decryptedMessage = $encryptedMessage . " (decrypted with RSA)";
-        
-        return $decryptedMessage;
-    }
+
+    private function getHiddenMessage($imageFile) {
+		// Load the image
+		$image = imagecreatefromstring(file_get_contents($imageFile));
+
+		// Get the image size
+		$width = imagesx($image);
+		$height = imagesy($image);
+
+		$hiddenMessage = '';
+
+		// Extract hidden message from the LSB of each pixel
+		for ($y = 0; $y < $height; $y++) {
+			for ($x = 0; $x < $width; $x++) {
+				$rgb = imagecolorat($image, $x, $y);
+				$r = ($rgb >> 16) & 0xFF;
+				$g = ($rgb >> 8) & 0xFF;
+				$b = $rgb & 0xFF;
+
+				// Extract the least significant bit from each color component
+				$redLSB = $r & 1;
+				$greenLSB = $g & 1;
+				$blueLSB = $b & 1;
+
+				// Combine the LSBs to form the binary representation of the hidden message
+				$binaryChar = $redLSB . $greenLSB . $blueLSB;
+
+				// Append the binary character to the hidden message
+				$hiddenMessage .= $binaryChar;
+
+				// Break the loop if the end of the message is reached
+				if (substr($hiddenMessage, -16) === '0000000000000000') {
+					break 2;
+				}
+			}
+		}
+
+		return $hiddenMessage;
+	}
+
 }
--- application/views/dekripsi.php
+++ application/views/dekripsi.php
@@ -1,25 +1,32 @@
 <?= $this->session->flashdata('pesan'); ?>
     <div class="card-body">
-    				<form class="form-horizontal" method="post" action="./dekripsi/process" enctype="multipart/form-data">
-                        <fieldset>
-                                    <div class="form-group">
-                                        <label class="col-lg-2 control-label" style="color:#333;" for="inputFile">Input Stego Image</label>
-                                        <div class="col-lg-4">
-                                            <input class="form-control" id="inputFile" placeholder="Input File" type="file" name="image" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-lg-2 control-label" style="color:#333;" for="inputPassword">Private Key</label>
-                                        <div class="col-lg-4">
-                                            <input class="form-control" id="inputPassword" type="text" placeholder="(d,n)" name="private_key" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-lg-2 control-label" for="textArea"></label>
-                                        <div class="col-lg-2">
-                                            <input type="submit" name="encrypt_now" value="Dekrip File dan Ekstrak" class="form-control btn btn-info">
-                                        </div>
-                                    </div>
-                        </fieldset>
-                    </form>
+		<form class="form-horizontal" method="post" action="./dekripsi/process" enctype="multipart/form-data">
+			<fieldset>
+						<div class="form-group">
+							<label class="col-lg-2 control-label" style="color:#333;" for="inputFile">Input Stego Image</label>
+							<div class="col-lg-4">
+								<input class="form-control" id="inputFile" placeholder="Input File" type="file" name="image" accept=".gif, .jpg, .jpeg, .png" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-lg-2 control-label" style="color:#333;" for="inputPassword">Private Key</label>
+							<div class="col-lg-4">
+								<input class="form-control" id="inputPassword" type="text" placeholder="(d,n)" name="private_key" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-lg-2 control-label" for="textArea"></label>
+							<div class="col-lg-2">
+								<input type="submit" name="encrypt_now" value="Dekrip File dan Ekstrak" class="form-control btn btn-info">
+							</div>
+						</div>
+			</fieldset>
+		</form>
+
+		<?php if ($this->session->flashdata('hidden_message')) { ?>
+			<div class="alert alert-success">
+				<strong>Hidden Message:</strong> <?php echo $this->session->flashdata('hidden_message'); ?>
+			</div>
+		<?php } ?>
+
     </div>
