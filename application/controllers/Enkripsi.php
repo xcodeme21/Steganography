69a4a08af5438816d95c7cb48aef738aea027c00
Agus Siswanto <agus.siswanto@erajaya.com>
2023-07-01 11:17:13 +0700

    update enkripsi

--- application/controllers/Enkripsi.php
+++ application/controllers/Enkripsi.php
@@ -36,80 +36,99 @@ class Enkripsi extends CI_Controller {
     public function process() {
         // Mendapatkan data dari formulir
         $publicKey = $this->input->post('pwdfile');
-        
+
         // Validasi format pwdfile (misalnya: (1234,1378))
         if (!preg_match('/^\(\d+,\d+\)$/', $publicKey)) {
             echo "Format pwdfile tidak valid. Harap masukkan format yang benar, misalnya: (1234,1378)";
             return;
         }
-        
+
         // Memuat file Excel
         $excelFile = $_FILES['excel_file']['tmp_name'];
-        
+
         // Menggunakan library PhpSpreadsheet untuk membaca file Excel
-       	$reader = IOFactory::createReaderForFile($excelFile);
-		$spreadsheet = $reader->load($excelFile);
+        $reader = IOFactory::createReaderForFile($excelFile);
+        $spreadsheet = $reader->load($excelFile);
 
-		// Mendapatkan data dari file Excel
-		$data = $this->getDataFromExcel($spreadsheet);
+        // Mendapatkan data dari file Excel
+        $data = $this->getDataFromExcel($spreadsheet);
 
-		// Mengonversi data menjadi RSA chipper
-		$messageToHide = $this->convertToRSA($data, $publicKey);
+        // Mengonversi data menjadi RSA chipper
+        $messageToHide = $this->convertToRSA($data, $publicKey);
 
-        
+
         // Melanjutkan proses enkripsi dan penyisipan
         $gambarFile = $_FILES['gambar_file']['tmp_name'];
         $outputFile = APPPATH . 'uploads/encrypt/' . $_FILES['gambar_file']['name'];
-        
+
         // Mengenkripsi pesan ke dalam gambar
         $this->encryptAndEmbed($gambarFile, $outputFile, $messageToHide);
-        
+
         $this->session->set_flashdata('success', "Enkripsi dan penyisipan berhasil!");
         redirect('enkripsi');
     }
-    
+
     private function getDataFromExcel($spreadsheet) {
         $data = array();
-        
+
         $worksheet = $spreadsheet->getActiveSheet();
         $highestRow = $worksheet->getHighestRow();
         $highestColumn = $worksheet->getHighestColumn();
-        
+
         for ($row = 1; $row <= $highestRow; $row++) {
             $rowData = $worksheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, null, true, false);
             $data[] = $rowData[0];
         }
-        
+
         return $data;
     }
-    
+
     private function convertToRSA($data, $publicKey) {
         // mengonversi data menjadi RSA chipper
         $message = serialize($data); // Mengubah data menjadi string terformat
-        $rsaChipper = $message . " (encrypted with RSA)";
-        
+        $rsaChipper = $message;
+
         return $rsaChipper;
     }
-    
+
+    private function convertToBinary($messageToHide) {
+        $binaryMessage = '';
+
+        $messageLength = strlen($messageToHide);
+        for ($i = 0; $i < $messageLength; $i++) {
+            $char = $messageToHide[$i];
+            $ascii = ord($char);
+            $binaryMessage .= sprintf("%08b", $ascii);
+        }
+
+        return $binaryMessage;
+    }
+
     private function encryptAndEmbed($sourceFile, $outputFile, $messageToHide) {
         // Mengambil konten gambar asli
         $imageContents = file_get_contents($sourceFile);
 
-        // Menyisipkan pesan ke dalam gambar menggunakan metode steganografi
-        $imageWithMessage = $this->hideMessageInImage($imageContents, $messageToHide);
+        // Mengonversi RSA chipper menjadi binary
+        $binaryMessage = $this->convertToBinary($messageToHide);
+
+        // Menyisipkan pesan binary ke dalam gambar menggunakan metode steganografi
+        $imageWithMessage = $this->hideMessageInImage($imageContents, $binaryMessage);
 
         // Menyimpan gambar dengan pesan yang telah disisipkan
-        file_put_contents($outputFile, $imageWithMessage);
+        if (file_put_contents($outputFile, $imageWithMessage) === false) {
+            echo "Gagal menyimpan gambar dengan pesan yang disisipkan.";
+            return;
+        }
     }
 
     private function hideMessageInImage($imageContents, $messageToHide) {
         // Mendeklarasikan variabel untuk menyimpan gambar hasil penyisipan pesan
         $imageWithMessage = '';
 
-        // Konversi pesan menjadi bitstream
-        $messageBits = $this->stringToBits($messageToHide);
+        // Konversi pesan menjadi binary
+        $messageBits = $messageToHide;
 
-        // Menyisipkan pesan ke dalam bit LSB (Least Significant Bit) dari setiap byte gambar
+        // Menyisipkan pesan binary ke dalam bit LSB (Least Significant Bit) dari setiap byte gambar
         $imageLength = strlen($imageContents);
         $messageLength = strlen($messageBits);
         $messageIndex = 0;
@@ -130,18 +149,5 @@ class Enkripsi extends CI_Controller {
         }
 
         return $imageWithMessage;
-    }
-
-    private function stringToBits($string) {
-        $bits = '';
-
-        $stringLength = strlen($string);
-        for ($i = 0; $i < $stringLength; $i++) {
-            $char = $string[$i];
-            $ascii = ord($char);
-            $bits .= sprintf("%08b", $ascii);
-        }
-
-        return $bits;
     }
 }
